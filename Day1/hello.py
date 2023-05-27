93a02bfe53bf77c0b5f6f6faa7491e3b4f547f25
Lyman <liming.swift@gmail.com>
2023-05-27 09:29:40 +0800

    Add files via upload
    day1 code

---
+++ Day1/hello.py
@@ -0,0 +1,29 @@
+# 这是注释
+import keyword
+
+# 获取保留字
+print(keyword.kwlist)
+
+print("Hello Python")
+
+if True:
+    print("true")
+    print("第二个 true")
+else:
+        print("false")
+        print("第二个 false")
+
+"""
+这是一个多行注释
+哈哈
+"""
+
+'''
+这又是一个多行注释
+嘿嘿
+'''
+
+哈哈 = 5 # 中文可以作为变量名
+print(哈哈)
+
+
